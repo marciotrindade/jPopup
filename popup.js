6f869ad852a16ab1443789c74e24aff0b9ee19d3
Marcio Trindade <marciotrindade@gmail.com>
2009-12-20 12:36:54 -0200

    add default values with extends of jQuery

--- popup.js
+++ popup.js
@@ -1,29 +1,29 @@
-/*
- *	jquery.popUp 1.0 - 2008-04-25
- *	http://www.webtrin.net/popup
- *	Copyright (c) Marcio Trindade
- *	Licence: LGPL
- *	Use: http://jquery.com/ and http://jquery.com/plugins/project/metadata
- */
 jQuery.fn.extend({
-  popup: function(options) {
+	popup: function(options) {
+		var defaults = {
+			name:'popup',
+			width:680,
+			height:460,
+			scroll:'no',
+			resize:'no'
+		};
+		options = $.extend({},defaults,options);
 		return this.each(function(){
 			if(!$(this).is("a")) return false;
 			
-			$(this).click(function(){
-				$.metadata.setType("class");
+			$(this).click(function(e) {
+				e.preventDefault();
 				var janela;
 				var url = $(this).attr("href");
-				var name = options.name||'popup';
-				var width = options.width||320;
-				var height = options.height||280;
+				var name = options.name;
+				var width = options.width;
+				var height = options.height;
 				var top = (screen.availHeight - height) / 2;
 				var left = (screen.availWidth - width) / 2;
-				var scroll = options.scroll||'no';
-				var resize = options.resize||'no';
-				var pop = window.open (url, name, 'width=' + width + ', height=' + height + ', top=' + top + ', left=' + left + ', scrollbars=' + scroll + ', resizable=' +  resize + ', statusbar=no');
+				var scroll = options.scroll;
+				var resize = options.resize;
+				var pop = window.open (url, name, 'width=' + width + ', height=' + height + ', top=' + top + ', left=' + left + ', scrollbars=' + scroll + ', resizable=' + resize + ', statusbar=no');
 				pop.focus();
-				return false;
 			});
 		});
 	}
